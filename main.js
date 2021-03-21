2935a719ad1f208dbe96c83b33361250c8ba94fe
Umar <umar.kt.24129@gmail.com>
2021-03-21 22:58:57 +0500

    second for submit html adding

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 app.get('/submittest',(req,res)=>{
     res.sendFile(__dirname + '/submittest.html')
 })
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000      //  const port = process.env.PORT   is for aws so that it can auto select provided port if other then 3000
 app.listen(port,()=>{
     console.log("WhatsUpp")
 })
