54747286fb07f4905219c85cb2acbb04b15898e6
Umar <umar.kt.24129@gmail.com>
2021-03-21 22:16:15 +0500

    port update for aws again

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ const app= express()
 app.get('/',(req,res)=>{
     res.send("Hello WOrld")
 })
-
+app.get('/test',(req,res)=>{
+    res.send("Test Hello WOrld")
+})
 const port = process.env.PORT || 3000
 app.listen(port,()=>{
     console.log("WhatsUpp")
