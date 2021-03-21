bdc2b465d218230ac3128a30e3d92f83d3ee7911
Umar <umar.kt.24129@gmail.com>
2021-03-21 22:34:29 +0500

    html adding

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ const express= require ('express')
 
 const app= express()
 
-app.get('/',(req,res)=>{
-    res.send("Hello WOrld")
-})
+// app.get('/',(req,res)=>{
+//     res.send("Hello WOrld")
+// })
 app.get('/test',(req,res)=>{
     res.send("Test Hello WOrld")
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '\\test.html')
 })
 const port = process.env.PORT || 3000
 app.listen(port,()=>{
